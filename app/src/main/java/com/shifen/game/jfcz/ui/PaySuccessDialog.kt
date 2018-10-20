b2404cf92c2873794657c9bca9e41447b06b259f
suzhangjiegh <su2010yx@163.com>
2018-10-20 15:18:39 +0800

    修改bug

--- app/src/main/java/com/shifen/game/jfcz/ui/PaySuccessDialog.kt
+++ app/src/main/java/com/shifen/game/jfcz/ui/PaySuccessDialog.kt
@@ -1,7 +1,9 @@
 package com.shifen.game.jfcz.ui
 
 import android.app.Activity
+import android.os.Build
 import android.os.Bundle
+import android.view.View
 import com.bumptech.glide.Glide
 import com.shifen.game.jfcz.R
 import com.shifen.game.jfcz.model.Goods
@@ -29,5 +31,23 @@ class PaySuccessDialog : Activity() {
         tvOrderNumber.text = getString(R.string.order_id, intent.getStringExtra(PayActivity.SESSION_ID_KEY))
 
         btnFinish.setOnClickListener { finish() }
+    }
+
+    override fun onAttachedToWindow() {
+        hideBottomUIMenu()
+    }
+    /**
+     * 隐藏虚拟按键，并且全屏
+     */
+    private fun hideBottomUIMenu() {
+        //隐藏虚拟按键，并且全屏
+        val decorView = window.decorView
+        if (Build.VERSION.SDK_INT > 11 && Build.VERSION.SDK_INT < 19) { // lower api
+            decorView.systemUiVisibility = View.GONE
+        } else if (Build.VERSION.SDK_INT >= 19) {
+            //for new api versions.
+            val uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or View.SYSTEM_UI_FLAG_FULLSCREEN
+            decorView.systemUiVisibility = uiOptions;
+        }
     }
 }
