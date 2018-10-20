e2edb3ad9fe2ceaae223c5c12c7406403c4888c6
suzhangjiegh <su2010yx@163.com>
2018-10-20 14:42:14 +0800

    修改bug

--- app/src/main/java/com/shifen/game/jfcz/ui/ErrorDialog.kt
+++ app/src/main/java/com/shifen/game/jfcz/ui/ErrorDialog.kt
@@ -4,8 +4,6 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
-import android.os.CountDownTimer
-import android.util.Log
 import android.view.MotionEvent
 import android.view.View
 import com.shifen.game.jfcz.R
