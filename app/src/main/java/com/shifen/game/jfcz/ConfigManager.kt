ff596d59a4f047957c05a3765eae200d760f9621
suzhangjiegh <su2010yx@163.com>
2018-10-30 14:28:14 +0800

    修改bug

--- app/src/main/java/com/shifen/game/jfcz/ConfigManager.kt
+++ app/src/main/java/com/shifen/game/jfcz/ConfigManager.kt
@@ -6,9 +6,7 @@ import android.content.SharedPreferences
 import android.util.Log
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
-import com.shifen.game.jfcz.model.Banner
-import com.shifen.game.jfcz.model.GameConfig
-import com.shifen.game.jfcz.model.Gift
+import com.shifen.game.jfcz.model.*
 
 
 /**
@@ -25,30 +23,47 @@ object ConfigManager {
     private const val key_game_cofing = "game_config"
 
     private const val key_gift_list = "gift_list"
-
+    private const val key_game_number = "key_game_number"
+    private const val key_net_game_number= "key_net_game_number"
     private const val key_banner_list = "banner_list"
+    private const val key_game_probability="key_game_probability"
 
     private lateinit var mContext: Context
     private lateinit var mConfigSP: SharedPreferences
     private var mGson = Gson()
-
     private const val mDefaultGameConfigJsonStr = "[" +
             "{\"createTime\":0,\"gameId\":1,\"checkPointLevel\":1,\"id\":1,\"kineves\":5,\"leaveTime\":15,\"rotate\":0,\"speed\":2,\"updateTime\":0}," +
-            "{\"createTime\":0,\"gameId\":2,\"checkPointLevel\":2,\"id\":2,\"kineves\":10,\"leaveTime\":13,\"rotate\":1,\"speed\":2,\"updateTime\":0}," +
-            "{\"createTime\":0,\"gameId\":3,\"checkPointLevel\":3,\"id\":3,\"kineves\":15,\"leaveTime\":10,\"rotate\":1,\"speed\":3,\"updateTime\":0}]"
+            "{\"createTime\":0,\"gameId\":2,\"checkPointLevel\":2,\"id\":2,\"kineves\":8,\"leaveTime\":13,\"rotate\":1,\"speed\":2,\"updateTime\":0}," +
+            "{\"createTime\":0,\"gameId\":3,\"checkPointLevel\":3,\"id\":3,\"kineves\":11,\"leaveTime\":10,\"rotate\":1,\"speed\":3,\"updateTime\":0}]"
+
+
+    private const val easyGameConfigJsonStr = "[" +
+            "{\"createTime\":0,\"gameId\":1,\"checkPointLevel\":1,\"id\":1,\"kineves\":5,\"leaveTime\":15,\"rotate\":0,\"speed\":2,\"updateTime\":0}," +
+            "{\"createTime\":0,\"gameId\":2,\"checkPointLevel\":2,\"id\":2,\"kineves\":8,\"leaveTime\":13,\"rotate\":0,\"speed\":2,\"updateTime\":0}," +
+            "{\"createTime\":0,\"gameId\":3,\"checkPointLevel\":3,\"id\":3,\"kineves\":11,\"leaveTime\":10,\"rotate\":0,\"speed\":2,\"updateTime\":0}]"
+
     private val mDefaultGameConfig: ArrayList<GameConfig>
     private var mGameConfig: List<GameConfig>? = null
 
     private var mGiftList: List<Gift>? = null
     private var mBannerList: List<Banner>? = null
 
+    private var mDefaultGameProbability:GameProbability?=null
+
     init {
         mDefaultGameConfig = mGson.fromJson(mDefaultGameConfigJsonStr, object : TypeToken<List<GameConfig>>() {}.type)
+        mDefaultGameProbability = GameProbability(4,68,1)
     }
 
     fun init(context: Context) {
         mContext = context
         mConfigSP = context.getSharedPreferences("config", Context.MODE_PRIVATE)
+    }
+
+    fun getEasyGameConfig():List<GameConfig> {
+        var mGameConfig: ArrayList<GameConfig>
+        mGameConfig = mGson.fromJson(easyGameConfigJsonStr, object : TypeToken<List<GameConfig>>() {}.type)
+        return  mGameConfig
     }
 
     fun getGameConfig(): List<GameConfig> {
@@ -110,5 +125,39 @@ object ConfigManager {
         mConfigSP.edit().apply {
             putString(key_banner_list, mGson.toJson(mBannerList))
         }.apply()
+    }
+
+
+    fun getGameNumber():Int{
+        var num =mConfigSP.getInt(key_game_number, getGameProbability()!!.rate)
+        return num
+    }
+
+    fun updateGameNumber(gameNumber: Int){
+        mConfigSP.edit().apply{
+            putInt(key_game_number,gameNumber)
+        }.apply()
+    }
+
+    fun updateGameProbability(probability: GameProbability) {
+        mConfigSP.edit().apply {
+            putString(key_game_probability, mGson.toJson(probability))
+        }.apply()
+
+        if (probability.randomType ==1){
+            updateGameNumber(probability.rate)
+        }
+    }
+
+    fun getGameProbability(): GameProbability ?{
+
+        var g : GameProbability ?=null;
+        val jsonStr = mConfigSP.getString(key_game_probability, "")
+        if (jsonStr.equals("")){
+            g= mDefaultGameProbability
+        }else{
+            g = mGson.fromJson(jsonStr,GameProbability::class.java)
+        }
+        return g
     }
 }
